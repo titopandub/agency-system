0e817cba95c8d40bccf2cfcfc5cafbd7448d02f6
titopa <yamatokusanagi@gmail.com>
2010-12-14 12:19:46 +0700

    Fix for add & delete table on Tug (Ports/show.html)

--- public/javascripts/addtablerow.js
+++ public/javascripts/addtablerow.js
@@ -5,28 +5,33 @@ function addRow(tableID) {
 	var rowI = rowCount - 1;
 	var row = table.insertRow(rowCount);
 	
-	var cell1 = row.insertCell(0);
+	var cell0 = row.insertCell(0);
+    var element0 = document.createElement("input");
+    element0.type = "checkbox";
+    cell0.appendChild(element0);
+	
+	var cell1 = row.insertCell(1);
 	var element1 = document.createElement("input");
 	element1.type = "text";
 	element1.name = 'tug[' + rowI + '].minimum';
 	element1.value = "";
 	cell1.appendChild(element1);
 	
-	var cell2 = row.insertCell(1);
+	var cell2 = row.insertCell(2);
 	var element2 = document.createElement("input");
 	element2.type = "text";
 	element2.name = 'tug[' + rowI + '].maximum';
 	element2.value = "";
 	cell2.appendChild(element2);
 	
-	var cell3 = row.insertCell(2);
+	var cell3 = row.insertCell(3);
 	var element3 = document.createElement("input");
 	element3.type = "text";
 	element3.name = 'tug[' + rowI + '].fixed';
 	element3.value = "";
 	cell3.appendChild(element3);
 	
-	var cell4 = row.insertCell(3);
+	var cell4 = row.insertCell(4);
 	var element4 = document.createElement("input");
 	element4.type = "text";
 	element4.name = 'tug[' + rowI + '].var';
@@ -36,11 +41,32 @@ function addRow(tableID) {
 	
 function deleteRow(tableID) {
     try {
-	    var table = document.getElementById(tableID);
-	    var rowCount = table.rows.length;
-	    var rowI = rowCount - 1;
-	    var deletecell = table.deleteRow(rowI);
+		var table = document.getElementById(tableID);
+		var rowCount = table.rows.length;
+ 
+        for(var i=0; i<rowCount; i++) {
+            var row = table.rows[i];
+            var chkbox = row.cells[0].childNodes[0];
+            if(null != chkbox && true == chkbox.checked) {
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+            }
+        }
+		
+		for(var j=0; j<rowCount; j++) {
+			var row = table.rows[j];
+			var input1 = row.cells[1].childNodes[0];
+			var input2 = row.cells[2].childNodes[0];
+			var input3 = row.cells[3].childNodes[0];
+			var input4 = row.cells[4].childNodes[0];
+			var jIndex = j - 1;
+			input1.name = 'tug[' + jIndex + '].minimum';
+			input2.name = 'tug[' + jIndex + '].maximum';
+			input3.name = 'tug[' + jIndex + '].fixed';
+			input4.name = 'tug[' + jIndex + '].var';
+		}
     } catch(e) {
-        alert(e);
+		alert(e);
     }
 }
